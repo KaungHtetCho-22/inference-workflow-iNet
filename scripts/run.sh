7755cc418929f76da10f7001179422fabdc7b417
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2025-08-13 16:04:52 +0700

    changed script name

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,10 @@ mkdir -p "$LOG_DIR"
 AUDIO_LOG="$LOG_DIR/audio_inference.log"
 PROCESS_LOG="$LOG_DIR/daily_report.log"
 
-python3 src/debug_audio_monitoring.py > "$AUDIO_LOG" 2>&1 &
+python3 src/audio_monitoring.py > "$AUDIO_LOG" 2>&1 &
 PID1=$!
 
-python3 src/debug_process_detections.py > "$PROCESS_LOG" 2>&1 &
+python3 src/process_detections.py --schedule > "$PROCESS_LOG" 2>&1 &
 PID2=$!
 
 tail -F "$AUDIO_LOG" "$PROCESS_LOG" &
